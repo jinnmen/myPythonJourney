e20bad4ba3ea939d84012537e943f9d48fd8bb42
jimmybb <jinnmen@gmail.com>
2017-11-06 05:40:13 +0000

    +1

---
+++ Mod4/WhileTrue/+1task.py
@@ -0,0 +1,20 @@
+votes = 3
+print(votes)
+
+votes+=2
+print(votes)
+
+print(votes)
+
+votes-=1
+print(votes)
+print()
+
+seat_count=0
+while True:
+    print("seat count: ",seat_count)
+    seat_count=seat_count+1
+    
+    if seat_count>4:
+        break
+    
