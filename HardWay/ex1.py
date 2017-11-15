c2dee4277589f7af450b950ab5b503e07d3c42ef
jimmybb <jinnmen@gmail.com>
2017-11-15 04:05:06 +0000

    hard start

---
+++ HardWay/ex1.py
@@ -0,0 +1,11 @@
+#print "Hello World!"
+#print  "Hello Again"
+#print "I like typing this."
+#print "This is fun."
+#print 'Yay! Printing.'
+#print "I'd much rather you 'not'."
+#print 'I "said" do not touch this.'
+
+print "Happy Wednesdays!"
+
+print "An octothorpe is also called a 'pound', 'hash','mesh',or any number of names. Pick one that makes you chill out."
