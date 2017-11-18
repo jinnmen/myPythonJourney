3374d18529c1564c2b8fdcaf24a69748d734774e
jimmybb <jinnmen@gmail.com>
2017-11-18 14:06:34 +0000

    ex5 completed

--- HardWay/ex5.py
+++ HardWay/ex5.py
@@ -6,9 +6,12 @@ eyes = 'Blue'
 teeth = 'White'
 hair = 'Brown'
 
+cm = height/2.54 #inches to cm
+kg = weight/0.453592 #pound to kg
+
 print "Let's talk about %s" % name
-print "He's %i inches tall." % height
-print "He's %i pounds heavy." % weight
+print "He's %i inches tall. Or %.2f cm tall" % (height, cm)
+print "He's %i pounds heavy. Or %.4f kg heavy" % (weight, kg)
 print "Actually that's not too heavy. "
 print "He's got %r eyes and %r hair." % (eyes, hair)
 print "His teeth are usually %s depending on the coffee. " % teeth
@@ -16,3 +19,7 @@ print "His teeth are usually %s depending on the coffee. " % teeth
 # this line is tricky, try to get it exactly right
 print "If I add %d, %d, and %d I get %d. " % (
 	age, height, weight, age + height + weight)
+print "Alternatively, in cm and kg, if you add %d, %6f, and %6f, you'd get %8f " %(
+	age, cm, kg, age + cm + kg) # %.6f or %6f produces same results. i=d for integer decimal, s for string.
+	
+#python format characters: https://docs.python.org/2.4/lib/typesseq-strings.html
