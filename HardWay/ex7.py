d0511288910969696218d963528f283ab8cc71a2
jimmybb <jinnmen@gmail.com>
2017-11-20 04:03:17 +0000

    fin ex7

--- HardWay/ex7.py
+++ HardWay/ex7.py
@@ -1,20 +1,13 @@
-print "Mary had a little lamb."
-print "Its fleece was shite as %s." % 'snow'
-print "And everywhere that Mary went."
-print "." * 10 # what'd that do? --> print ..........
+#repr = evaluatable string representation of an object. / raw programmers version of variable
+formatter = "%r %r %r %r"
 
-end1 = "C"
-end2 = "h"
-end3 = "e"
-end4 = "e"
-end5 = "s"
-end6 = "e"
-end7 = "B"
-end8 = "u"
-end9 = "r"
-end10 = "g"
-end11 = "e"
-end12 = "r"
-
-print end1 + end2 + end3 + end4 + end5 + end6,
-print end7 + end8 + end9 +end10 + end11 + end12
+print formatter % (1, 2, 3, 4)
+print formatter % ("one", "two", "three", "four")
+print formatter % (True, False, False, True)
+print formatter % (formatter, formatter, formatter, formatter)
+print formatter % (
+	"I had this thing. ", #double quotes won't show when executed because no ' inside string.
+	"That you could type up right. ",
+	"But it didn't sing. ", #double quotes will be shown because of ' inside double quotes.
+	"So I said goodnight."
+)
