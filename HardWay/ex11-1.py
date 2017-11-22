f2656955d85d4c8fd520a7644dff7d84814bbe15
jimmybb <jinnmen@gmail.com>
2017-11-22 05:00:31 +0000

    ex11-1 added

---
+++ HardWay/ex11-1.py
@@ -0,0 +1,8 @@
+print "What's the weather today?"
+weather = raw_input()
+print "Say something random?"
+random = raw_input()
+dog = raw_input("Do you own a dog?")
+num = int(input("Key in a number"))
+
+print weather , random, dog, num
