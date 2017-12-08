43db53583d507d25e91fc7abf8c4ebbd44875023
jimmybb <jinnmen@gmail.com>
2017-12-08 04:52:40 +0000

    edited ex20

--- HardWay/ex20.py
+++ HardWay/ex20.py
@@ -5,11 +5,11 @@ from sys import argv
 script, input_file = argv
 
 # defines function print_all with parameter f
-def print_all(f):
+def print_all(f): 
 	print f.read() #prints parameter f with read function which returns specified amount of bytes from file.
 	
 def rewind (f): # defines rewind with parameter f
-	f.seek(0) # adds functions seek to set the file's current position to start reading from 0.  
+	f.seek(0) # adds functions seek to set the file's current position to start reading from 0 = from beginning of file, 1 = current position, 2 = end of file.   f = file. 
 	
 def print_a_line(line_count, f): # defines print a line
 	print line_count, f.readline() # print line_count, reads one entire line from file. http://python-reference.readthedocs.io/en/latest/docs/file/readline.html?highlight=readline
@@ -27,8 +27,8 @@ rewind(current_file) #repeats the file contents
 print "Let's print three lines:" #prints string
 
 current_line = 1 #assigns first line
-print_a_line(current_line, current_file) # sets which line and file to print
+print_a_line(current_line, current_file) # sets which line and file to print . Current_line =1
 
 current_line = current_line + 1
-print_a_line(current_line, current_file) # prints second line and file
+print_a_line(current_line, current_file) # prints second line and file.  Current_line =2
 
