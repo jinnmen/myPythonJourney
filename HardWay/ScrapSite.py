08de15bdd9a5988dd9885a9644f9181f163d417a
jimmybb <jinnmen@gmail.com>
2017-12-11 14:35:30 +0000

    updated ScrapSite.py

--- HardWay/ScrapSite.py
+++ HardWay/ScrapSite.py
@@ -3,6 +3,9 @@
 import urllib2
 from bs4 import BeautifulSoup
 
+import csv
+from datetime import datetime
+
 # specify the url
 quote_page = 'http://yealink.com/news.html'
 
@@ -13,12 +16,20 @@ page = urllib2.urlopen(quote_page)
 soup = BeautifulSoup(page, 'html.parser')
 
 # Take out the <div> of name and get its value
-name_box = soup.find('h1', attrs={'class': 'name'})
+name_box = soup.find('div', attrs={'class': 'name'})
 
-name = name_box.text.strip() # strip() is used to remoev starting and trailing 
+name = name_box.text.strip() # strip() is used to remove starting and trailing 
 print name
 
 # get the index price
-price_box = soup.find('div', attrs={'class': 'price'})
-price = price_box.text
-print price
+## price_box = soup.find('div', attrs={'class': 'price'})
+## price = price_box.text
+## print price
+
+# get the contents
+
+# Export to Excel CSV
+# open a csv file with append, so old data will not be erased
+with open('index.csv', 'a') as csv_file:
+ writer = csv.writer(csv_file)
+ writer.writerow([name, datetime.now()])
