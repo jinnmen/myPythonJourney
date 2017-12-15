1b2d2eebe2cb80085aa4da6466b78f9eb410129a
jimmybb <jinnmen@gmail.com>
2017-12-15 04:32:33 +0000

    updated BS4 ex1

--- Projects/ex1.py
+++ Projects/ex1.py
@@ -6,4 +6,25 @@ myfile = open ("/Users/l.jinnmen/Documents/Qloog/pyway-master/J/ScrapSite/threes
 data = myfile.read()
 soup = BeautifulSoup(data,'html.parser')
 
-print(soup.prettify())
+#print(soup.prettify())
+
+#print soup.title
+
+#print soup.title.name
+
+#print soup.title.string
+
+#print soup.title.parent.name
+
+#print soup.p
+
+#print soup.p['class']
+
+#print soup.a
+
+#print soup.find_all('a')
+
+print soup.find(id = "link3")
+
+for link in soup.find_all('a'):
+    print(link.get('href'))
