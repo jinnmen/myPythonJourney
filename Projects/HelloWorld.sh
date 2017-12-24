32d97a78e95471d3adff5d7cc5bd124b86ec3b5d
jimmybb <jinnmen@gmail.com>
2017-12-24 03:51:29 +0000

    added new shellscript

---
+++ Projects/HelloWorld.sh
@@ -0,0 +1,5 @@
+#! /bin/bash  <---find this by using "which bash" in command to find root folder for bash
+echo "Hello World"
+
+#ls -al in the bash root folder only allows write, read but not executable "x" by default hence need to use "chmod -x hello.sh" to make it executable.
+# Next use "./hello.sh" to run script in command.
