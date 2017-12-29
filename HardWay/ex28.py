b670b1b3b00ebe3caa410cfa3e625357864d3a2e
jimmybb <jinnmen@gmail.com>
2017-12-29 00:03:11 +0000

    completed ex28

---
+++ HardWay/ex28.py
@@ -0,0 +1,76 @@
+"""
+1. True
+2. False
+3. True and False = False
+4. True
+5. True or True = True
+6. True and True = True
+7. False and True = False
+8. True or True = True
+9. False
+10. True and False = False
+11. True
+12. False
+13. not (False) = True
+14. not (True and True) = False
+15. not (False or True) = False
+16. not (True or False) = False
+17. not (True and False) = True
+18. True and not (Fakse or False) = True
+19. False and not (False or True) = False
+20. True and not (True or False) = False
+"""
+
+1. True and True
+2. False and True
+3. 1==1and2==1 4. "test" == "test"
+5. 1==1or2!=1
+6. Trueand1==1
+7. False and 0 != 0
+8. Trueor1==1
+9. "test" == "testing"
+10. 1!=0and2==1
+11. "test" != "testing"
+12. "test" == 1
+13. not (True and False)
+14. not(1==1and0!=1)
+15. not (10 == 1 or 1000 == 1000)
+16. not(1!=10or3==4)
+17. not ("testing" == "testing" and "Zed" == "Cool Guy")
+18. 1==1andnot("testing"==1or1==0)
+19. "chunky" == "bacon" and not (3 == 4 or 3 == 3)
+20. 3 == 3 and not ("testing" == "testing" or "Python" == "Fun")
+
+
+True and True
+False and True
+1==1 and 2==1 
+"test" == "test"
+1==1 or 2!=1
+True and 1==1
+False and 0 != 0
+True or 1==1
+"test" == "testing"
+1!=0 and 2==1
+"test" != "testing"
+"test" == 1
+not (True and False)
+not(1==1 and 0!=1)
+not (10 == 1 or 1000 == 1000)
+not(1!=10 or 3==4)
+not ("testing" == "testing" and "Zed" == "Cool Guy")
+1==1 and not ("testing"==1 or 1==0)
+"chunky" == "bacon" and not (3 == 4 or 3 == 3)
+3 == 3 and not ("testing" == "testing" or "Python" == "Fun")
+
+
+"""
+Comparison operators/ equality operators
+!= not equal
+==equal
+>=greater than  equal
+<=less than and equal
+< less than
+> greater than
+<> not equal -> similar to !=
+"""
