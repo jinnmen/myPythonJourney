bee90b0f50b54acf780d17faf3eb871ddb047755
jimmybb <jinnmen@gmail.com>
2018-01-08 13:19:22 +0000

    added comments to ex35

--- HardWay/ex35.py
+++ HardWay/ex35.py
@@ -1,47 +1,47 @@
-from sys import exit
+from sys import exit # import the function exit from system
 
-def gold_room():
-	print "This room is full of gold. How much do you take?"
+def gold_room(): #creates a new variable gold_room, step 4
+	print "This room is full of gold. How much do you take?" #prints string
 	
-	next = raw_input("> ")
-	if "0" in next or "1" in next:
-		how_much = int(next)
+	next = raw_input("> ") #creates next variable, asks users to key in.
+	if "0" in next or "1" in next: #if statement with 0 or 1.
+		how_much = int(next) # creates how_much variable, with integer next parameter
 	else:
-		dead("Man, learn to type a number.")
+		dead("Man, learn to type a number.") # other than numbers prints this string.
 		
-	if how_much < 50:
-		print "Nice, you're not greedy, you win!"
-		exit(0)
-	else:
+	if how_much < 50: #if less than 50
+		print "Nice, you're not greedy, you win!" #prints string.
+		exit(0) #exits code
+	else: # otherwise prints string within dead var (along with dead var)
 		dead("You greedy bastard!")
 		
-def bear_room():
+def bear_room(): #creates step 2 to move bear
 	print "There is a bear here."
 	print "The bear has a bunch of honey."
 	print "The fat bear is in front of another door."
 	print "How are you going to move the bear?"
-	bear_moved = False
+	bear_moved = False # sets bear_moved variable default as False
 	
-	while True:
-		next = raw_input("> ")
+	while True: # creates while loop
+		next = raw_input("> ") #asks question for next var under bear_room indentation.
 		
-		if next == "take honey":
-			dead("The bear looks at you then slaps your face off.")
-		elif next == "taunt bear" and not bear_moved:
-			print "The bear has moved from door. You can go through it now."
-			bear_moved = True
-		elif next == "taunt bear" and bear_moved:
+		if next == "take honey": #if ans is take honey
+			dead("The bear looks at you then slaps your face off.") #reply with string then dead's string
+		elif next == "taunt bear" and not bear_moved: #if taunt bear, and not False
+			print "The bear has moved from door. You can go through it now." # bear moved
+			bear_moved = True #sets bear_moved to True, perhaps won't need this
+		elif next == "taunt bear" and bear_moved: # if taunt bear 2nd time and False
 			dead("The bear gets pissed off and chews your leg off.")
-		elif next == "open door" and bear_moved:
+		elif next == "open door" and bear_moved: #open door to enter gold room
 			gold_room()
 		else:
-			print "I got no idea what that means."
+			print "I got no idea what that means." #if type anything else asides from above settings.
 			
-def cthulhu_room():
+def cthulhu_room(): #creates var cthulhu_room. When first turned right
 	print "Here you see the great evil Cthulu."
 	print "He, it, whatever stares at you and you go insane."
 	print "Do you flee for your life or eat your head?"
-	next = raw_input("> ")
+	next = raw_input("> ") #allows user to key in.
 	
 	if "flee" in next:
 		start()
@@ -50,11 +50,11 @@ def cthulhu_room():
 	else:
 		cthulhu_room()
 		
-def dead(why):
-	print why, "Good job!"
-	exit(0)
+def dead(why): #defines var dead with parameter why
+	print why, "Good job!"  #whys above + this extra sentence.
+	exit(0) #exits.
 	
-def start():
+def start(): #step 1, start.
 	print "You are in a dark room."
 	print "There is a door to your right and left."
 	print "Which one do you take?"
@@ -66,6 +66,6 @@ def start():
 	elif next == "right":
 		cthulhu_room()
 	else:
-		dead("You stumble around the room until you starve.")
+		dead("You stumble around the room until you starve.") #dead, game over.
 		
-start()
+start() #starts function
