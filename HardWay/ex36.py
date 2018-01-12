09af749095ffdbb589cab44db77aa4085c23f0b4
jimmybb <jinnmen@gmail.com>
2018-01-12 11:22:40 +0000

    ex 36 updated

--- HardWay/ex36.py
+++ HardWay/ex36.py
@@ -1,6 +1,7 @@
 items = ['shield', 'sword', 'garlic']
-items = False
-#gow() = False
+# items = False
+items_picked = []
+# gow() = False
 life = True
 
 def gow():
@@ -28,16 +29,35 @@ def in_castle():
 			print "You see 3 items on the table."
 			print "A shield, a sword, and some garlic"
 			print "What do you do?"
-			
-			
-	
+		
+		while True:
+			next = raw_input("> ")
+
+			if "shield".lower() in next:
+				print "You picked up a shield of the sun!"
+				items_picked.append("shield")
+				items[0]
+				print items_picked
+				print items_picked[0]
+			elif "sword".lower() in next:
+				print "You picked up a sword of truth!"
+				items_picked.append("sword")
+				items[1]
+			elif "garlic".lower() in next:
+				print "You picked up garlic."
+				items_picked.append("garlic")
+				items[2]
+			else:
+				print "I'm not sure what you want to do"
+		
+
 
 
 def silver_room():
 	print "Welcome to the silver room"
 	
 def die(end):
-	end = "Game over."
-	print die(end)
-	
+	print "You're dead!", end
+	exit(0)
+		
 gow()
