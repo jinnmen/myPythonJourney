6fa797726edb0134c3b66fe4e015cac28bcb2bbd
jimmybb <jinnmen@gmail.com>
2018-01-21 11:50:59 +0000

    added ex38 points

--- HardWay/ex38.py
+++ HardWay/ex38.py
@@ -20,3 +20,15 @@ print stuff[-1] # whoa! fancy
 print stuff.pop()
 print ' '.join(stuff) # what? cool!
 print '#'.join(stuff[3:5]) # super stellar!
+
+"""
+1. Take each function that is called, and go through the steps outlined above to translate them to what Python does.
+For example, ' '.join*things) is join(' ', things).
+2. Translate these two ways to view the function calls in English. For example, ' '.join(things) reads as "Join things with '' between them." 
+Meanwhile, join(' ',things) means, "Call join with '' and things." Understand how they are really the same thing.
+3. Go read about "Object Oriented Programming" online. Confused? I was too. Do not worry. You will learn enough to be dangerous, and you can slowly learn more later.
+4. Read up on what a "class" is in Python. Do not read about how other languages use the word "class". That will only mess you up.
+# Read up on class vs instances. Keywords: _init_
+5. What's the relationship between dir(something) and the "class" of something?
+6 Find out OOP vs functional programming. 
+"""
