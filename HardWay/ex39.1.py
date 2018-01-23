da445f418b4319858c6446a607810411e28a9b50
jimmybb <jinnmen@gmail.com>
2018-01-23 00:08:59 +0000

    added ex39.1

---
+++ HardWay/ex39.1.py
@@ -0,0 +1,43 @@
+# Create a mapping of Prefecture abbreviation
+pref = {
+	'Aichi': 'AIC',
+	'Ehime': 'EHI',
+	'Gunma': 'GUN',
+	'Hokkaido': 'HOK',
+	'Hyogo': 'HYO',
+	'Ibaraki': 'IBA',
+	'Ishikawa': 'ISH',
+	'Iwate': 'IWA',
+	'Kagawa': 'KAG',
+	'Kanagawa': 'KAN',
+	'Mie': 'MIE',
+	'Miyagi': 'MIY',
+	'Osaka': 'OSA',
+	'Shiga': 'SHI',
+	'Tochigi': 'TOC',
+	'Yamanashi': 'YAM'
+}
+
+# create a basic set of prefectures and their capital cities
+cities = {
+	'AIC' = 'Nagoya'
+	'EHI': 'Matsuyama',
+	'GUN': 'Maebashi',
+	'HOK': 'Sapporo',
+	'HYO': 'Kobe',
+	'IBA': 'Mito',
+	'ISH': 'Kanazawa',
+	'IWA': 'Morioka',
+	'KAG': 'Takamatsu',
+	'KAN': 'Yokohama',
+	'MIE': 'Tsu',
+	'MIY': 'Sendai',
+	'OSA': 'Osaka',
+	'SHI': 'Otsu',
+	'TOC': 'Utsunomiya',
+	'YAM': 'Kofu'
+}
+
+# add some more cities
+cities['TOC'] = 'Utsunomiya'
+cities['YAM'] = 'Kofu'
