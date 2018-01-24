b2aa5a3f38c10eeaab12078ee0ef5fd78870ff2f
jimmybb <jinnmen@gmail.com>
2018-01-24 04:21:00 +0000

    completed ex39.1

--- HardWay/ex39.1.py
+++ HardWay/ex39.1.py
@@ -1,5 +1,5 @@
 # Create a mapping of Prefecture abbreviation
-pref = {
+prefs = {
 	'Aichi': 'AIC',
 	'Ehime': 'EHI',
 	'Gunma': 'GUN',
@@ -20,7 +20,7 @@ pref = {
 
 # create a basic set of prefectures and their capital cities
 cities = {
-	'AIC' = 'Nagoya'
+	'AIC': 'Nagoya',
 	'EHI': 'Matsuyama',
 	'GUN': 'Maebashi',
 	'HOK': 'Sapporo',
@@ -34,10 +34,48 @@ cities = {
 	'MIY': 'Sendai',
 	'OSA': 'Osaka',
 	'SHI': 'Otsu',
-	'TOC': 'Utsunomiya',
-	'YAM': 'Kofu'
 }
 
 # add some more cities
 cities['TOC'] = 'Utsunomiya'
 cities['YAM'] = 'Kofu'
+
+# print out some cities
+print '-' * 10
+print "Aichi prefecture has: ", cities['AIC']
+print "Ehime prefecture has: ", cities['EHI']
+print "Gunma prefecture has: ", cities['GUN']
+print "Hokkaido prefecture has: ", cities['HOK']
+
+# print some prefecture
+print '-' * 10
+print "Aichi's abbreviation is: ", prefs["Aichi"]
+print "Ehime's abbreviation is: ", prefs['Ehime']
+print "Gunma's abbreviation is: ", prefs['Gunma']
+print "Hokkaido's abbreviation is: ", prefs['Hokkaido']
+
+# do it by using the prefecture then cities dict
+print '-' * 10
+print "Aichi has: ", cities[prefs['Aichi']]
+print "Ehime has ", cities[prefs['Ehime']]
+
+# print every prefecture's abbreviation
+print '-' * 10
+for pref, abbrev in prefs.items():
+	print "%s is abbreviated as %s" % (pref, abbrev)
+	
+# print every city in the prefectures
+print '-' * 10
+for abbrev, pref in prefs.items():
+	print "%s prefecture is abbreviated %s and has city %s" % (pref, abbrev, prefs[abbrev])
+	
+print '-' * 10
+# safely get an abbreviation by prefecture that might not be there (Shimane=SHI, city= Matsune)
+pref = prefs.get('Shimane', None)
+
+if not pref:
+	print "Sorry, no Shimane"
+
+# get a pref with a default value
+pref = prefs.get('SHI', 'Does not Exist')
+print "The city for the prefecture 'SHI' is %s" % pref
