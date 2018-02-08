514d896794292441dd7f53478fa489d1b5e0a251
jimmybb <jinnmen@gmail.com>
2018-02-08 00:28:57 +0000

    added ex43

---
+++ HardWay/ex43.py
@@ -0,0 +1,42 @@
+"""
+Write out nouns/ draw problem. 
+A lil' paragraph for the game:
+Aliens have invaded a space ship and our hero has to go through a maze of rooms defeating them so he can escape into an escape pod to the planet below. 
+The game will be more like a Zrok or Adventure type game with text outputs and funny ways to die.
+The game wil involve an engine that runs a map full of rooms or scenes. Each room will print its own description when the
+player enters it and then tell the engine what room to run next out of the map.
+
+Describe each scene:
+Death: When player dies and should be something funny.
+Central Corridor: Starting point and has Gothon already standing there they have to defeat with a joke before continuing
+Laser Weapon Armory: When hero gets neutron bomb to blow up ship before getting escape pod. Keypad and has to guess number.
+The Bridge: Another battle sene with Gothon where hero places bomb.
+Escape Pod: Hero escapes but only guessin right pod.
+
+Make a list of all nouns:
+Alien
+Player
+Ship
+Maze
+Room
+Scene
+Gothon
+Escape od
+Planet
+Map
+Engine
+Death
+Central Corridor
+Laser Weapon Armory
+The Bridge
+
+Could possibly go through all verbs to see if any good function names but skip for now.
+
+Make class hierarchy
+* Map
+* Engine
+* Scene
+	* Death
+	* Central Corridor
+	
+"""
