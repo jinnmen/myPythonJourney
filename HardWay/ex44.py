83495c04cadd90b389ec5c46945f990f3680090e
jimmybb <jinnmen@gmail.com>
2018-02-19 13:07:15 +0000

    Updated ex44

--- HardWay/ex44.py
+++ HardWay/ex44.py
@@ -13,8 +13,6 @@ son = Child()
 
 dad.implicit()
 son.implicit()
-"""
-
 
 # Override Explicitly
 
@@ -33,3 +31,22 @@ son = Child()
 
 dad.override()
 son.override()
+"""
+
+class Parent(object):
+	
+	def altered(self):
+		print "PARENT altered()"
+
+class Child(Parent):
+
+	def altered(self):
+		print "CHILD, BEFORE PARENT altered()"
+		super(Child, self).altered() #Use super to get parent altered version. Call super with arguments Child and self, then call the function altered on whatever it returns.
+		print "CHILD, AFTER PARENT altered()"
+		
+dad = Parent()
+son = Child()
+
+dad.altered()
+son.altered()
