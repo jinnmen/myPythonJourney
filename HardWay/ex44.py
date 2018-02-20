5f584c326c1ea8f63d54899cf327a21fc89b9cfd
jimmybb <jinnmen@gmail.com>
2018-02-20 00:07:49 +0000

    updated ex44

--- HardWay/ex44.py
+++ HardWay/ex44.py
@@ -31,7 +31,6 @@ son = Child()
 
 dad.override()
 son.override()
-"""
 
 class Parent(object):
 	
@@ -47,6 +46,41 @@ class Child(Parent):
 		
 dad = Parent()
 son = Child()
+
+dad.altered()
+son.altered()
+
+"""
+
+class Parent(object):
+
+	def override(self):
+		print "PARENT override()"
+		
+	def implicit(self):
+		print "PARENT implicit()"
+		
+	def altered(self):
+		print "PARENT altered()"
+		
+class Child(Parent):
+
+	def override(self):
+		print "CHILD override()"
+		
+	def altered(self):
+		print "CHILD, BEFORE PARENT altered()"
+		super(Child, self).altered()
+		print "CHILD, AFTER PARENT altered()"
+		
+dad = Parent()
+son = Child()
+
+dad.implicit()
+son.implicit()
+
+dad.override()
+son.override()
 
 dad.altered()
 son.altered()
