b48d7aa36d036d48b6e44402e386f5f67a01b53d
jimmybb <jinnmen@gmail.com>
2018-03-06 00:28:20 +0000

    Added ReadFileSimple

---
+++ HardWay/ReadFileSimple.py
@@ -0,0 +1,5 @@
+file = open("/Users/tomokohakata/Documents/jimmy/Python/pyway-master/Js/ex45.txt", "r")
+print file.read()
+
+#http://www.pythonforbeginners.com/files/reading-and-writing-files-in-python
+#https://www.guru99.com/reading-and-writing-files-in-python.html
