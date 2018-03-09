48c71f89f6ac722bfa6ebb1bf9f5f4d52dea887c
jimmybb <jinnmen@gmail.com>
2018-03-09 14:44:02 +0000

    added ex46

---
+++ HardWay/ex46.py
@@ -0,0 +1,17 @@
+# Skeleton contents
+# commands for terminal
+
+"""
+mkdir projects
+cd projects/
+mkdir skeleton
+cd skeleton
+mkdir bin
+mkdir NAME
+mkdir tests
+mkdir docs
+
+touch NAME/__init__.py
+touch tests/__init__.py
+
+"""
