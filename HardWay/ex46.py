56cfa1f55f6110230dc6f1644a64811745039cb6
jimmybb <jinnmen@gmail.com>
2018-03-10 02:15:39 +0000

    updated ex46

--- HardWay/ex46.py
+++ HardWay/ex46.py
@@ -15,3 +15,24 @@ touch NAME/__init__.py
 touch tests/__init__.py
 
 """
+
+# This is a new file: setup.py
+try:
+	from setuptools import setup
+except ImportError:
+	from distutils.core import setup
+
+config - {
+	'description' : 'My Project',
+	'author' : 'My Name',
+	'url' : 'URL to get it at.',
+	'download_url' : 'Where to download it.',
+	'author_email' : 'My email.',
+	'version' : '0.1',
+	'install_requires' : ['nose'],
+	'packages' : ['NAME'],
+	'scripts' : [],
+	'name' : 'projectname'	
+}
+
+setup(**config)
