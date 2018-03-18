addd636f073057fd2626a91f0b3fa81dae2c86e3
jimmybb <jinnmen@gmail.com>
2018-03-18 14:09:58 +0000

    updated lexicon.py

--- HardWay/lexicon.py
+++ HardWay/lexicon.py
@@ -1,3 +1,4 @@
+
 class lexicon(object):
 
 	def __init__(self, name, description):
@@ -13,3 +14,33 @@ class lexicon(object):
 		
 	def scan(self, everything):
 		return self.name.get(everything, None)
+		
+# https://stackoverflow.com/questions/15424895/creating-lexicon-and-scanner-in-python
+
+lexicon ={
+    ('directions', 'north'),
+    ('directions', 'south'),
+    ('directions', 'east'),
+    ('directions', 'west'),
+    ('verbs', 'go'),
+    ('verbs', 'stop'),
+    ('verbs', 'look'),
+    ('verbs', 'give'),
+    ('stops', 'the'),
+    ('stops', 'in'),
+    ('stops', 'of'),
+    ('stops', 'from'),
+    ('stops', 'at')
+    }
+
+def scan(sentence):
+
+    words = sentence.lower().split()
+    pairs = []
+
+    for word in words:
+        word_type = lexicon[word]
+        tupes = (word, word_type) 
+        pairs.append(tupes)
+
+    return pairs
