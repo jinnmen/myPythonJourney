21c75decd5eb7e2288d97106cb2fad664f6636dc
jimmybb <jinnmen@gmail.com>
2018-03-22 01:15:46 +0000

    Added SeleniumTestSimple

---
+++ Projects/SeleniumTestSimple.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome("/Users/l.jinnmen/Documents/python/chromedriver")
+
+driver.set_page_load_timeout("10")
+driver.get("http://google.com")
+driver.find_element_by_name("q").send_keys("Automation Step by Step")
+driver.find_element_by_name("btnK").click()
+time.sleep(4)
+driver.quit
