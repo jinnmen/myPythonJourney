8248207646bb3cf98b1b07b612e9cbd6a5d9a67f
jimmybb <jinnmen@gmail.com>
2018-03-24 01:37:54 +0000

    Updated SeleniumTestSimple

--- Projects/SeleniumTestSimple.py
+++ Projects/SeleniumTestSimple.py
@@ -8,8 +8,12 @@ driver.get("http://google.com")
 driver.find_element_by_name("q").send_keys("Automation Step by Step")
 driver.find_element_by_name("btnK").click()
 time.sleep(4)
-driver.quit
+driver.close
+#driver.quit
 
 print "Test completed successfully"
 
 # https://www.youtube.com/watch?v=FFDDN1C1MEQ
+
+# Detailed tests and explanations: https://media.readthedocs.org/pdf/selenium-python/latest/selenium-python.pdf
+# Test module __main__ explained: https://stackoverflow.com/questions/419163/what-does-if-name-main-do
