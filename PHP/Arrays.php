65d6139566d300f62df8363a9f986efa970dc7bc
jimmybb <jinnmen@gmail.com>
2018-06-04 09:49:34 +0000

    Updated Arrays

--- PHP/Arrays.php
+++ PHP/Arrays.php
@@ -41,3 +41,17 @@
     </p>
   </body>
 </html>
+
+
+<html>
+  <head>
+    <title>Leap Years</title>
+  </head>
+  <body>
+    <?php
+      for ($leap = 2004; $leap < 2050; $leap = $leap + 4) {
+        echo "<p>$leap</p>";
+      }
+    ?>
+  </body>
+</html>
