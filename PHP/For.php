866c053145caf3b24e72bb049e13c79a14027bb9
jimmybb <jinnmen@gmail.com>
2018-06-07 00:23:25 +0000

    Updated For.php

---
+++ PHP/For.php
@@ -0,0 +1,31 @@
+<html>
+  <head>
+    <title>For Loops</title>
+  </head>
+  <body>
+    <p>
+      <?php
+        // Echoes the first five even numbers
+        for ($i = 2; $i < 11; $i = $i + 2) {
+          echo $i;
+        }
+      ?>
+    </p>
+  </body>
+</html>
+
+<html>
+  <head>
+    <title>Solo For Loop!</title>
+  </head>
+  <body>
+    <p>
+      <?php
+      // Write your for loop below!
+        for ($i = 0; $i < 101; $i = $i + 10){
+            echo $i;
+        }
+      ?>
+    </p>
+  </body>
+</html>
