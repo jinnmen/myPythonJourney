0f406d47397f36a563dbf6b639ae95b7c880ea79
jimmybb <jinnmen@gmail.com>
2018-06-13 01:42:42 +0000

    Updated Form.php

--- PHP/Form.php
+++ PHP/Form.php
@@ -79,9 +79,9 @@
             Comment: <textarea name = "comment" rows = "5" cols = "40"><?php echo $comment;?></textarea><br>
             
             Gender:
-            <input type = "radio" name "gender" <?php if (isset($gender) && $gender == "female") echo "checked";?> value = "female"> Female
-            <input type = "radio" name "gender" <?php if(isset($gender) && $gender == "male") echo "checked"; ?> value = "male"> Male
-            <input type = "radio" name "gender" <?php if(isset($gender) && $gender =="other") echo "checked"; ?>value = "other"> Other
+            <input type = "radio" name = "gender" <?php if (isset($gender) && $gender == "female") echo "checked";?> value = "female"> Female
+            <input type = "radio" name = "gender" <?php if(isset($gender) && $gender == "male") echo "checked"; ?> value = "male"> Male
+            <input type = "radio" name = "gender" <?php if(isset($gender) && $gender =="other") echo "checked"; ?>value = "other"> Other
             <span class = "error">* <?php echo $genderErr;?></span>
             <br><br>
             <input type = "submit" name = "submit" value = "Submit">
