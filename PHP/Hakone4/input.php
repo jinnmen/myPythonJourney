96fd18ea3941fa64cbc9f7f413f006ed35b2dce9
jimmybb <jinnmen@gmail.com>
2018-07-25 00:58:54 +0000

    Updated input.php

--- PHP/Hakone4/input.php
+++ PHP/Hakone4/input.php
@@ -11,19 +11,19 @@ if (!empty($_POST["name"])){
 }
 
 if (!empty($_POST["email"])){
-	$email = $_POST["email"]
+	$email = $_POST["email"];
 }
 
 if (!empty($_POST["password"])){
-	$password = $_POST["password"]
+	$password = $_POST["password"];
 }
 
 if (!empty($_POST["url"])){
-	$url = $_POST["url"]
+	$url = $_POST["url"];
 }
 
 if (!empty($_POST["comment"])){
-	$comment = $_POST["comment"]
+	$comment = $_POST["comment"];
 }
 
 ?>
@@ -31,7 +31,7 @@ if (!empty($_POST["comment"])){
 
 
 <!doctype html>
-<!-- http://54.199.142.88/bs/bsform.php 
+<!-- http://54.199.142.88/bs/bsform.php
 Grid: https://getbootstrap.com/docs/4.1/layout/grid/
 -->
 
@@ -45,95 +45,94 @@ Grid: https://getbootstrap.com/docs/4.1/layout/grid/
    </head>
    <body>
 	  <div class = "container">
-	  
 
-      
       <div class = "row">
          <div class = "col-md-2"></div>
       </div>
       <form>
          <div class = "row justify-content-center">
             <div class = "col-md-8">
-            
+
               <div class="form-group">
                 <div class = "row justify-content-center">
       			 <h1> Hello, BS world! </h1>
               	</div>
-              	
+
               	<div class = "row justify-content-center">
               	<div class = "col-md-4">
                  <label for="nameInput">Name</label>
                  </div>
-                 
+
                  <div class = "col-md-4">
                  <input type="name" class="form-control" id="nameInput" aria-describedby="nameHelp" placeholder="Enter Name" value= "<?php echo $name; ?>">
-            
+
                  </div>
                  </div>
-              	
-              	
-              	
+
+
+
               	<div class = "row justify-content-center">
               	<div class = "col-md-4">
                  <label for="emailInput">Email address</label>
                  </div>
-                 
+
                  <div class = "col-md-4">
                  <input type="email" class="form-control" id="emailInput" aria-describedby="emailHelp" placeholder="Enter email">
                  <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
                  </div>
                  </div>
-              </div>
-              
+
+
               <div class = "row justify-content-center">
               <div class = "col-md-4">
               <label for="passwordInput">Password</label>
               </div>
-              
+
               <div class = "col-md-4">
               <input type="password" class="form-control" id="passwordInput" placeholder="Password">
-                         
+
               <div class="form-group form-check">
                 <input type="checkbox" class="form-check-input" id="exampleCheck1">
                 <label class="form-check-label" for="exampleCheck1">Agree to T&Cs</label>
                 </div>
                 </div>
-                         	
+
               	<div class = "row justify-content-center">
               	<div class = "col-md-4">
                  <label for="urlInput">Website URL</label>
                  </div>
-                 
+
                  <div class = "col-md-4">
                  <input type="url" class="form-control" id="urlInput" aria-describedby="urlHelp" placeholder="Enter URL">
-            
+
                  </div>
                  </div>
-                 
+
                 <div class = "row justify-content-center">
               	<div class = "col-md-4">
                  <label for="commentInput">Comment</label>
                  </div>
-                 
+
                  <div class = "col-md-4">
                  <input type="comment" class="form-control" id="commentInput" aria-describedby="commentHelp" placeholder="Enter Comment">
-            
+
                  </div>
                  </div>
-                 
-              </div>      <!--Closing for row justify content center -->        
-              
-              
+
+              </div>      <!--Closing for row justify content center -->
+
+
               <div class = "row justify-content-center">
               <button type="submit" class="btn btn-primary">Submit</button>
               </div>
+							</div>
            </div> <!--closing for col md 8 -->
          </div>
       </form>
       <div class = "row">
          <div class = "col-md-2"></div>
       </div>
-      
+
       </div>
       <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
