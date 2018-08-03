5150cf365186b6895823c59e6d10900988553fe7
jimmybb <jinnmen@gmail.com>
2018-08-03 08:48:03 +0000

    Updated input and confirm with js

--- PHP/Hakone4/confirm.php
+++ PHP/Hakone4/confirm.php
@@ -75,90 +75,114 @@ if (!empty($error_lists)) {
 </head>
 <body>
 <div class = "container">
-  <div class = "col-md-2">
-  </div>
-
+  <div class = "col-md-2"></div>
   <div class = "row justify-content-center">
      <div class = "col-md-8">
-
        <div class="form-group">
          <div class = "row justify-content-center">
-      <h1> Confirm.php page! </h1>
+           <h1> Confirm.php page! </h1>
          </div>
+         <!--Button for form sends to send.php page, posts the information there, sets name as test -->
+         <form action="send.php" method="post" name="test" id= "confirm">
 
-<!--Button for form sends to send.php page, posts the information there, sets name as test -->
-  <form action="send.php" method="post" name="test">
+          <div class = "row justify-content-center">
+            <div class = "col-md-4">
+              Name :
+            </div>
+            <div class = "col-md-4">
+              <?php echo $name; ?> <br />
+            </div>
+          </div>
 
-    <div class = "row justify-content-center">
-    <div class = "col-md-4">
-    Name :
-    </div>
-    <div class = "col-md-4">
-    <?php echo $name; ?> <br />
-  </div>
-</div>
+          <div class = "row justify-content-center">
+            <div class = "col-md-4">
+              Email :
+            </div>
+            <div class = "col-md-4">
+              <?php echo $email; ?> <br />
+            </div>
+          </div>
 
-                <div class = "row justify-content-center">
-      <div class = "col-md-4">
-    Email :
-  </div>
-        <div class = "col-md-4">
-    <?php echo $email; ?> <br />
-  </div>
-</div>
+          <div class = "row justify-content-center">
+            <div class = "col-md-4">
+              Password:
+            </div>
+            <div class = "col-md-4">
+              <?php echo $password ;?> <br />
+            </div>
+          </div>
 
-  <div class = "row justify-content-center">
-    <div class = "col-md-4">
-    Password:
-</div>
-<div class = "col-md-4">
-    <?php echo $password ;?> <br />
-  </div>
-</div>
+          <div class = "row justify-content-center">
+            <div class = "col-md-4">
+              Url :
+            </div>
+            <div class = "col-md-4">
+              <?php echo $url; ?> <br />
+            </div>
+          </div>
 
-<div class = "row justify-content-center">
-  <div class = "col-md-4">
-    Url :
-  </div>
-  <div class = "col-md-4">
-    <?php echo $url; ?> <br />
-  </div>
-</div>
+          <div class = "row justify-content-center">
+            <div class = "col-md-4">
+              Comment:
+            </div>
+            <div class = "col-md-4">
+              <?php echo $comment ?> <br />
+            </div>
+          </div>
 
-    <div class = "row justify-content-center">
-    <div class = "col-md-4">
-    Comment:
-  </div>
-    <div class = "col-md-4">
-    <?php echo $comment ?> <br />
-  </div>
-</div>
+          <div class = "row justify-content-center">
+            <div class = "col-md-4">
 
-    <div class = "row justify-content-center">
-    <div class = "col-md-4">
-    <input type="submit" name="submit" class="btn btn-primary" value="send" />
-  </div>
-    </form>
+            </div>
+          </div>
+          <input name="name" type="hidden" value="<?php echo $name; ?>" />
+          <input name="email" type="hidden" value="<?php echo $email; ?>" />
+          <input name="password" type ="hidden" value = "<?php echo $password; ?>" />
+          <input name="url" type = "hidden" value = "<?php echo $url; ?>" />
+          <input name ="comment" type = "hidden" value = "<?php echo $comment; ?>" />
+        </form>
+
+        <button name="submit" class="btn btn-primary" id="back_button">back</button>
+        <button name="submit" class="btn btn-primary" id="send_button">send</button>
 
 <!--Button for form sends to input.php page, posts the information there, sets name as test? -->
-  <form action="input.php" method="post" name="test">
-    <input name="name" type="hidden" value="<?php echo $name; ?>" />
-    <input name="email" type="hidden" value="<?php echo $email; ?>" />
-    <input name="password" type ="hidden" value = "<?php echo $password; ?>" />
-    <input name="url" type = "hidden" value = "<?php echo $url; ?>" />
-    <input name ="comment" type = "hidden" value = "<?php echo $comment; ?>" />
-      <div class = "col-md-4">
-    <input type="submit" name="submit" class="btn btn-primary" value="back" />
-  </div>
+        <form action="input.php" method="post" name="test" id= "back">
+          <input name="name" type="hidden" value="<?php echo $name; ?>" />
+          <input name="email" type="hidden" value="<?php echo $email; ?>" />
+          <input name="password" type ="hidden" value = "<?php echo $password; ?>" />
+          <input name="url" type = "hidden" value = "<?php echo $url; ?>" />
+          <input name ="comment" type = "hidden" value = "<?php echo $comment; ?>" />
+        </form>
 
-    <div class = "col-md-2">
+        <div class = "col-md-4">
+
+        </div>
+
+        <div class = "col-md-2">
+        </div>
+      </div>
     </div>
   </div>
 </div>
-</div>
-  </form>
-  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
+<script>
+
+$(function(){
+
+  $("#send_button").on("click", function(){
+    var form_data = $("#confirm");
+    form_data.submit();
+  });
+
+  $("#back_button").on("click", function(){
+    var form_data = $("#back");
+    form_data.submit();
+  });
+
+});
+
+</script>
 </body>
 </html>
--- PHP/Hakone4/input.php
+++ PHP/Hakone4/input.php
@@ -121,7 +121,7 @@ Grid: https://getbootstrap.com/docs/4.1/layout/grid/
                  </div>
 
                  <div class = "col-md-4">
-                 <input type="comment" name = "comment" class="form-control" id="comment" aria-describedby="commentHelp" placeholder="Enter Comment" value= "<?php echo $comment; ?>" />
+                 <textarea name = "comment" class="form-control" rows="3" id="comment" aria-describedby="commentHelp" placeholder="Enter Comment"><?php echo $comment; ?> </textarea>
 
                  </div>
                  </div>
@@ -141,7 +141,7 @@ Grid: https://getbootstrap.com/docs/4.1/layout/grid/
       </div>
 
       </div>
-			</form?
+		</form>
       <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
       <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.2/js/bootstrap.min.js" integrity="sha384-o+RDsa0aLu++PJvFqy8fFScvbHFLtbvScb8AjopnFD+iEQ7wo/CG0xlczd+2O/em" crossorigin="anonymous"></script>
