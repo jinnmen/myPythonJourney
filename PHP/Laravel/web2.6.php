6cce2187879c8c275d1f1b6124a92bcb5b60a3a3
jimmybb <jinnmen@gmail.com>
2018-11-01 00:59:07 +0000

    Added web2.6.php

---
+++ PHP/Laravel/web2.6.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+use App\Task;
+
+
+Route::get('/tasks', function () {
+
+	$tasks = Task::all();
+
+	return view('tasks.index',compact('tasks'));
+
+});
+
+
+Route::get('/tasks/{task}', function ($id){
+
+	$task = App\Task::find($id);
+
+	dd($task);
+
+	return view('tasks.show', compact('tasks'));
+
+});
