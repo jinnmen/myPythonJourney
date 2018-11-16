6ed516b958626ee41aabe0daf64eefe4362ba8e6
jimmybb <jinnmen@gmail.com>
2018-11-16 00:57:02 +0000

    Updated indexblade

--- PHP/Laravel/index2.blade.php
+++ PHP/Laravel/index2.blade.php
@@ -46,4 +46,39 @@
 				@endif
 			</div>
 			<div class="form-group required {{ $errors->has('subject') ? 'has-error' : '' }}">
-				
+				<label class="control-label" for ="subject">件名</label>
+				@if(old('confirming','false') === 'false')
+					<input type = "text" class= "form-control" name = "subject" value = "{{ old('subject') }}">
+				@else
+					<p class = "form-control-static">{{ old('subject')}}></p>
+				@endif
+				@if(#errors->has('subject'))
+					<p class="help-block">{{ $errors->first('subject')}}</p>
+				@endif
+			</div>
+			<div class = "form-group required {{ $errors->has('content') ? 'has-error' : ''}}">
+				<label class = "control-label" for ="content">内容</label>
+				@if(old('confirming', 'false') === 'false')
+					<textarea type="text" class ="form-control" name="content" rows="10">{{
+						old('content')
+					}}</textarea>
+				@else
+					<p class="form-control-static">{!! nl2br(e(old('content'))) !!}</p>
+					<input type = "hidden" name="content" value="{{old('content') }}">
+				@endif
+				@if($errors->has('content'))
+					<p class="help-block">{{ $errors->first('content')}}</p>
+				@endif
+			</div>
+			<div class="form-group text-center">
+				@if(old('confirming', 'false') === 'false')
+					<button type="submit" class = "btn btn-primary">確認</button>
+				@else
+					<button type="submit" name="action" value="post" class="btn btn-primary">送信</button>
+					<button type="submit" name="action" value="back" class="btn btn-default">戻る</button>
+				@endif
+			</div>
+		{!! Form::close() !!}
+	</div>
+</div>
+@endsection 
